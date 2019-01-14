2be0dccf523d383aa840e3c8ec2f4ee7d5af08e3
Cole Marsteller and Katrina Hockman <cole+katrina@>
2019-01-14 14:31:13 -0800

    kind of add deck of cards and draw method

--- src/game.js
+++ src/game.js
@@ -1,21 +1,32 @@
 import { Disease } from './disease.js';
 import { City } from './city.js';
+import { Player } from './player.js';
 
 export class Game {
   constructor(totalPop){
     this.totalPop = totalPop;
     this.cities = [];
     this.diseases;
+    this.researchPoints = 0;
+    // this.cardDeck = {researchPointCards:{value:[1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]}, scientistCards:{gainScientist:[1,1,1,1,2]}, consequenceCards:{loseScientist:[1,1,1], diseaseMutation:[1,2,3], loseReasearchStation:[1]}};
+    this.cardDeck = {1:'research+1',2:'research+1',3:'research+1',4:'research+1',5:'research+1',6:'gain1Scientist',7:'gain1Scientist',8:'gain1Scientist',9:'gain1Scientist',10:'lose1Scientist',11:'lose1Scientist',12:'diseaseMutation',13:'loseReasearchStation'};
+
+    // this.cardDeckTest = {Rcards:{value:[1,2,3,4]}}
+    // console.log(this.cardDeckTest);
   }
 
 
   startGame() {
+    let player = new Player();
+    console.log(player);
     let disease = new Disease('Ebola');
     disease.diseaseType();
     console.log(disease);
     this.generateCities(4, disease);
     this._breakoutCheck();
     this.diseaseSpread(disease);
+    player.drawCard(this.cardDeck);
+    console.log(player.hand);
     // this.cities[0].infected = true;
   }
 
@@ -40,7 +51,7 @@ export class Game {
       this.cities.forEach((city) => {
         console.log((Date.now() - disease.lastCityInfected));
         if (Date.now() - disease.lastCityInfected > 20000) {
-          console.log(city.infected);
+          console.log(city.name, city.infected, city.sickPop, city.deadPop);
           let number =  Math.floor((Math.random() * 10000));
           if(number < (1000 - disease.infectionRate)) {
             city.infected = true;
@@ -50,5 +61,11 @@ export class Game {
       });
     }, 5000);
   }
+
+  // _parseDeck() {
+  //   let parsedDeck = [];
+  //   parsedDeck = parsedDeck.concat(this.cardDeck.scientistCards.gainScientist, this.cardDeck.researchPointCards.value, this.cardDeck.consequenceCards.loseScientist, this.cardDeck.consequenceCards.diseaseMutation, this.cardDeck.consequenceCards.loseReasearchStation)
+  //   return parsedDeck;
+  // }
 
 }
--- src/player.js
+++ src/player.js
@@ -1,7 +1,37 @@
 export class Player {
   constructor() {
     this.role;
-    this.hand;
+    this.hand = [];
     this.turns;
   }
+
+  // const cardDeck = {researchPointCards:{value:{1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5}}, scientistCards:{gainScientist:{1,1,1,1,2}}, consequenceCards:{loseScientist:{1,1,1}, diseaseMutation:{1,2,3}, loseReasearchStation:{1}}};
+
+  drawCard(deck) {
+    if (this.hand.length < 3) {
+      let card = Math.floor((Math.random() * 13) + 1)
+      console.log(card);
+      console.log(deck);
+      this.hand.push(deck[card]);
+    }
+  }
+
+  // drawCard(deck) {
+  //   let card = Math.floor((Math.random() * (deckLengths[0] + deckLengths[1] + deckLengths[2] + deckLengths[3] + deckLengths[4])))
+  //   console.log(card);
+  //   if (card <= deckLengths[0]) {
+  //     console.log('scientistCards');
+  //   } else if (card > deckLengths[0] && card <= deckLengths[1]) {
+  //     console.log('researchCards');
+  //   } else if (card > deckLengths[0]+deckLengths[1] && card  <= deckLengths[2]) {
+  //     console.log('loseScientist');
+  //   }  else if (card > deckLengths[0]+deckLengths[1]+deckLengths[2] && card  <= deckLengths[3]) {
+  //     console.log('diseaseMutation');
+  //   } else if (card > deckLengths[0]+deckLengths[1]+deckLengths[2]+deckLengths[3] && card  <= deckLengths[4]) {
+  //     console.log('loseReasearchStation');
+  //   }
+  // }
+
+
+
 }
